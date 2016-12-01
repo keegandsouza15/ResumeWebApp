03761305e9edf9b3fb4243f432b16f72e7538bf4
keegandsouza15 <keegan0415dsouza@gmail.com>
2016-11-30 19:11:12 -0800

    Keegan D'Souza CS 355 Lab 10

--- routes/account_routes.js
+++ routes/account_routes.js
@@ -36,5 +36,61 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new account form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    account_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('account/accountAdd', {'account': result});
+        }
+    });
+});
+
+// insert an account record
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.first_name == null) {
+        res.send('First name must be provided.');
+    }
+    else if(req.query.last_name == null) {
+        res.send('Last name must be provided.');
+    }
+    else if(req.query.email == null){
+        res.send('An email must be provided.');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        account_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/account/all');
+            }
+        });
+    }
+});
+
+// Delete an account for the given account_id
+router.get('/delete', function(req, res){
+    if(req.query.account_id == null) {
+        res.send('address_id is null');
+    }
+    else {
+        account_dal.delete(req.query.account_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/account/all');
+            }
+        });
+    }
+});
 
 module.exports = router;
