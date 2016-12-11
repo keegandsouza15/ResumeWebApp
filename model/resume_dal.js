a1129b5d37a483cff8d0dd09a1ca56eb3a6f0b53
keegandsouza15 <keegan0415dsouza@gmail.com>
2016-12-11 12:21:00 -0800

    Sunday 12:19 PM

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -37,6 +37,42 @@ exports.insert = function(params, callback) {
 
 }
 
+exports.insert1 = function(params, callback) {
+
+    // FIRST INSERT THE COMPANY
+    var query = 'INSERT INTO resume (resume_name, account_id) VALUES (?,?)';
+
+    var queryData = [params.resume_name, params.account_id];
+
+    connection.query(query, params.resume_name, function(err, result) {
+
+        // THEN USE THE COMPANY_ID RETURNED AS insertId AND THE SELECTED ADDRESS_IDs INTO COMPANY_ADDRESS
+        var resume_id = result.insertId;
+
+        // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
+        var query = 'INSERT INTO resume_skill (resume_id, skill_id) VALUES ?';
+
+        // TO BULK INSERT AN ARRAY OF VALUES WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+        var resume_skillData = [];
+        // if only one value is submitted, JavaScript will treat the value as an array, so we skip it if its not an array
+        // for example if the value of params.address_id was "10", it would loop over the "1" and then the "0", instead of
+        // treating it as one value.
+        if(params.skill_id instanceof Array) {
+            for(var i=0; i < params.skill_id.length; i++) {
+                resume_skillData.push([resume_id, params.skill_id[i]]);
+            }
+        }
+        else {
+            resume_skillData.push([resume_id, params.skill_id]);
+        }
+
+        // NOTE THE EXTRA [] AROUND companyAddressData
+        connection.query(query, [resume_skiillData], function(err, result){
+            callback(err, result);
+        });
+    });
+
+};
 exports.delete = function(resume_id, callback) {
     var query = 'DELETE FROM resume WHERE resume_id = ?';
     var queryData = [resume_id];
@@ -45,3 +81,87 @@ exports.delete = function(resume_id, callback) {
         callback(err, result);
     });
 }
+
+//declare the function so it can be used locally
+var resumeInsert = function(resume_id, accountIdArray, callback){
+    // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
+    var query = 'INSERT INTO resume (resume_id, account_id) VALUES ? , ?';
+
+    // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+    var resumeData = [];
+    for(var i=0; i < accountIdArray.length; i++) {
+        resumeData.push([resume_id, accountIdArray[i]]);
+    }
+    connection.query(query, [resumeData[i]], [resumeData[i]], function(err, result){
+        callback(err, result);
+    });
+};
+//export the same function so it can be used by external callers
+module.exports.resumeInsert = resumeInsert;
+
+//declare the function so it can be used locally
+var resumeDeleteAll = function(resume_id, callback){
+    var query = 'DELETE FROM resume WHERE resume_id = ?';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
+//export the same function so it can be used by external callers
+module.exports.resumeDeleteAll = resumeDeleteAll;
+
+exports.update = function(params, callback) {
+    var query = 'UPDATE resume SET resume_name = ?, account_id = ? WHERE resume_id = ?';
+    var queryData = [params.resume_name, params.account_id, params.resume_id];
+ /*   connection.query(query, queryData, function(err, result) {
+        //delete company_address entries for this company
+       resumeDeleteAll(params.resume_id, function(err, result){
+
+          if(params.account_id != null) {
+                //insert company_address ids
+               resumeInsert(params.resume_id, params.account_id, function(err, result){
+                    callback(err, result);
+               });}
+            else {
+                callback(err, result);
+            }
+        });
+
+    });*/
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
+
+/*  Stored procedure used in this example
+ DROP PROCEDURE IF EXISTS company_getinfo;
+
+ DELIMITER //
+ CREATE PROCEDURE company_getinfo (_company_id int)
+ BEGIN
+
+ SELECT * FROM company WHERE company_id = _company_id;
+
+ SELECT a.*, s.company_id FROM address a
+ LEFT JOIN company_address s on s.address_id = a.address_id AND company_id = _company_id
+ ORDER BY a.street, a.zipcode;
+
+ END //
+ DELIMITER ;
+
+ # Call the Stored Procedure
+ CALL company_getinfo (4);
+
+ */
+
+exports.edit = function(resume_id, callback) {
+    var query = 'CALL resume_getinfo(?)';
+    var queryData = [resume_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var resume_dal = require('../model/resume_dal');
 var account_dal = require('../model/account_dal');
+var skill_dal = require('../model/skill_dal');
+var company_dal = require('../model/company_dal');
+var school_dal = require('../model/school_dal');
 
 
 // View All resumes
@@ -24,12 +27,12 @@ router.get('/', function(req, res){
     }
     else {
         resume_dal.getById(req.query.resume_id, function(err,result) {
-           if (err) {
-               res.send(err);
-           }
-           else {
-               res.render('resume/resumeViewById', {'result': result});
-           }
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('resume/resumeViewById', {'result': result});
+            }
         });
     }
 });
@@ -38,12 +41,18 @@ router.get('/', function(req, res){
 router.get('/add', function(req, res){
     // passing all the query parameters (req.query) to the insert function instead of each individually
     account_dal.getAll(function(err,result) {
-        if (err) {
-            res.send(err);
-        }
-        else {
-            res.render('resume/resumeAdd', {'account': result});
-        }
+        skill_dal.getAll(function (err,skill) {
+            company_dal.getAll (function (err,company) {
+                school_dal.getAll(function (err, school) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    else {
+                        res.render('resume/resumeAdd', {'account': result, 'skill': skill, 'company': company,'school':school});
+                    }
+                });
+            });
+        });
     });
 });
 
@@ -87,6 +96,41 @@ router.get('/delete', function(req, res){
             }
         });
     }
+});
+
+router.get('/update', function(req, res) {
+    resume_dal.update(req.query, function(err, result){
+        res.redirect(302, '/resume/all');
+    });
+});
+
+
+
+router.get('/edit', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('A resume id is required');
+    }
+    else {
+        resume_dal.edit(req.query.resume_id, function(err, result){
+            console.log(result);
+            res.render('resume/resumeUpdate', {resume: result[0][0], account: result[1]});
+        });
+    }
+
+});
+
+router.get('/edit2', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('A resume id is required');
+    }
+    else {
+        resume_dal.getById(req.query.resume_id, function(err, company){
+            address_dal.getAll(function(err, address) {
+                res.render('resume/resumeUpdate', {resume: company[0], resume: address});
+            });
+        });
+    }
+
 });
 
 
